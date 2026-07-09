49eedafcb6fd66c1368493a2634feeaedc56f921
Nathan Kemmerer <your_email>
2026-07-08 21:42:14 -0700

    Fix Flutter server connection test

--- mobile/researchos_mobile/test/widget_test.dart
+++ mobile/researchos_mobile/test/widget_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:researchos_mobile/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('shows server connection screen', (tester) async {
+    SharedPreferences.setMockInitialValues({});
     await tester.pumpWidget(const ResearchOsMobileApp());
-    expect(find.text('Connect to ResearchOS'), findsOneWidget);
+    await tester.pumpAndSettle();
+
+    expect(find.text('ResearchOS'), findsOneWidget);
     expect(find.text('Server URL'), findsOneWidget);
+    expect(find.text('Test Connection'), findsOneWidget);
   });
 }
